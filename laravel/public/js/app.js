d548feb4ec3a753d7ba1a811d9c31c7230c68770
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2021-05-11 00:34:00 +0900

    sass-loaderのバージョン変更

--- laravel/public/js/app.js
+++ laravel/public/js/app.js
@@ -49946,7 +49946,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nValidationError: Invalid options object. Sass Loader has been initialized using an options object that does not match the API schema.\n - options has an unknown property 'outputStyle'. These properties are valid:\n   object { implementation?, sassOptions?, prependData?, sourceMap?, webpackImporter? }\n    at validate (/var/www/html/laravel/node_modules/schema-utils/dist/validate.js:98:11)\n    at Object.loader (/var/www/html/laravel/node_modules/sass-loader/dist/index.js:36:28)\n    at runLoaders (/var/www/html/laravel/node_modules/webpack/lib/NormalModule.js:316:20)\n    at /var/www/html/laravel/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /var/www/html/laravel/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at runSyncOrAsync (/var/www/html/laravel/node_modules/loader-runner/lib/LoaderRunner.js:143:3)\n    at iterateNormalLoaders (/var/www/html/laravel/node_modules/loader-runner/lib/LoaderRunner.js:232:2)\n    at /var/www/html/laravel/node_modules/loader-runner/lib/LoaderRunner.js:205:4\n    at process.nextTick (/var/www/html/laravel/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:85:15)\n    at process._tickCallback (internal/process/next_tick.js:61:11)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 
